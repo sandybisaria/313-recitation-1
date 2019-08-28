82505c2781da9d4fadbcacf963aa35b8223e0589
Syed Raziq Mohideen <mohideen.syed@rocketmail.com>
2019-08-28 14:05:57 -0400

    Print out years at CMU

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Hello, world! I've been here for 5 years.");
     }
 }
